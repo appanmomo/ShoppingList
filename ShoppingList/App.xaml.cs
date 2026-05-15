d0622e27bc8898f3cb4aa2a1c153595dadca748d
Kaylee Frank <frankaylee10@gmail.com>
2026-05-15 00:35:17 -0500

    Created new app, UI only :sparkles:

---
+++ ShoppingList/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingList.Views;
+
+namespace ShoppingList;
+
+public partial class App : Application
+{
+    public static string SessionKey = "";
+    
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage( new MainPage());
+    }
+}
